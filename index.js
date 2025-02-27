58e85c809d10f898f82770c6ca08591321a1a667
pallavi-270 <veeramanikantapallavi@gmail.com>
2025-02-27 20:15:08 +0530

    add styles

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("download").addEventListener("click", function () {
+    let pdfUrl = "https://drive.google.com/file/d/1gmaODV4449s4BsAp2LD6qGPiNtcXJJII/view?usp=sharing";
+    window.open(pdfUrl, "_blank");
+});
